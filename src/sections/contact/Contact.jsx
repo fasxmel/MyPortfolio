ae1c36b022df54828dbd974fa3774537e107fdee
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-10-20 10:00:13 +0200

    add func on contact form

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,12 +1,49 @@
 import styles from "./Contact.module.css";
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2'
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_iy4rikg', 
+        'template_j0ggf6z', 
+        form.current,
+         
+        {
+        publicKey: '0L2IDyhf6LfMUoOic',  //public key
+      })
+
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          Swal.fire({
+            title: "Success!",
+            text: "Your message has been sent. Thank you!",
+            icon: "success"
+          });
+          e.target.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+
+
+
+
   return (
     <section id="contact" className={styles.container}>
       <h1 className={styles.sectionTitle}>Contact</h1>
 
-      
-        <form>
+      {/* TODO: Add functionality to send email to resive email also */}
+        <form ref={form} onSubmit={sendEmail}>
           <div className={styles.formGroup}>
 
             <label htmlFor="name" hidden>
@@ -15,7 +52,7 @@ const Contact = () => {
             <input
             id="name" 
             type="text" 
-            name="name"
+            name="user_name"
             placeholder="Name" 
             required 
             />
@@ -25,19 +62,19 @@ const Contact = () => {
             </label>
             <input
             id="email" 
-            type="text" 
-            name="email"
+            type="email" 
+            name="user_email"
             placeholder="Email" 
             required 
             />
 
-            <label htmlFor="massage" hidden>
-               Massage
+            <label htmlFor="message" hidden>
+               Message
             </label>
             <textarea
-            id="massage" 
-            name="massage"
-            placeholder="Massage" 
+            id="message" 
+            name="message"
+            placeholder="Message" 
             required >
             </textarea>
 
